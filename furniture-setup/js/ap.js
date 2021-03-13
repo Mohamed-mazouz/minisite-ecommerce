3ce829407207a9ea491689dd68c32c98d75cca24
MAZOUZI-MOHAMED <medevinfo.2018@gmail.com>
2021-03-13 17:29:40 +0100

    minisite-ecommerce

---
+++ furniture-setup/js/ap.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+
+//     let activee = $("nav ul li a");
+//     activee.click(function(event) {
+//     event.preventDefault(); 
+//     activee.removeClass("active");
+//     $(this).addClass('active');
+
+// });
+
+// });
